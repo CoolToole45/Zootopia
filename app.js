af11fd95a55ce2894037b89b08646516c99b2ff3
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-03-27 18:34:50 +0400

    Almost done

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper2 = new Swiper(".mySwiper-2", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
