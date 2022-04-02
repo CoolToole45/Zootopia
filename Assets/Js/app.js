ed801bdb05888a264f99f498878e3504de80ffa9
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-04-02 17:22:42 +0400

    Complete...

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -1,4 +1,4 @@
-var mainSwiper = new Swiper(".main-swiper", {
+var mainSlider = new Swiper(".main-slider", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
@@ -12,10 +12,65 @@ var mainSwiper = new Swiper(".main-swiper", {
     },
 });
 
-var swiper2 = new Swiper(".brands-slider", {
+
+var productSlider = new Swiper(".product-slider", {
+  slidesPerView: 4,
+  spaceBetween: 50,
+  slidesPerGroup: 4,
+  loop: true,
+  autoplay: true,
+  loopFillGroupWithBlank: false,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".product-button-next",
+    prevEl: ".product-button-prev",
+  },
+});
+
+
+var productSliderMobile = new Swiper(".product-slider-mobile", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  slidesPerGroup: 1,
+  loop: true,
+  autoplay: true,
+  loopFillGroupWithBlank: false,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".product-button-next",
+    prevEl: ".product-button-prev",
+  },
+});
+
+
+var brandsSlider = new Swiper(".brands-slider", {
   slidesPerView: 5,
   spaceBetween: 50,
   slidesPerGroup: 5,
+  loop: true,
+  autoplay: true,
+  loopFillGroupWithBlank: false,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".brands-slide-button-next",
+    prevEl: ".brands-slide-button-prev",
+  },
+});
+
+
+var brandsSlider = new Swiper(".brands-slider-mobile", {
+  slidesPerView: 2,
+  spaceBetween: 50,
+  slidesPerGroup: 2,
   loop: true,
   autoplay: true,
   loopFillGroupWithBlank: false,
