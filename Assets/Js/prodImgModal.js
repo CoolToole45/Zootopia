757876d6ea8e86001350c140211da7e5fad78a4b
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-05-02 15:37:59 +0400

    single-prod mobile complete

--- Assets/Js/prodImgModal.js
+++ Assets/Js/prodImgModal.js
@@ -1,8 +1,4 @@
 function displayImgModal() {
     const modalDisplay = document.getElementById('modal-display');
-    if(modalDisplay.classList.contains('hidden')) {
-        modalDisplay.classList.remove('hidden');
-    } else {
-        modalDisplay.classList.add('hidden');
-    }
+    modalDisplay.classList.toggle('hidden');
 }
